936f05620127afe66f6730e11f867944ca1ab71a
phionox <77087690+phionox@users.noreply.github.com>
2025-11-15 18:44:34 -0500

    Delete pricing-page.js

--- pricing-page.js
+++
@@ -1,6 +0,0 @@
-function toggleDescription(element) {
-  const description = element.nextElementSibling.nextElementSibling;
-  description.style.display = description.style.display === 'block' ? 'none' : 'block';
-  element.style.color = '#ff6600';
-}
-
