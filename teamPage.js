9937269e329c48b6ba5d8e86b6c4cad00efb4968
phionox <77087690+phionox@users.noreply.github.com>
2025-11-16 15:52:46 -0500

    Delete teamPage.js

--- teamPage.js
+++
@@ -1,8 +0,0 @@
-const navItems = document.querySelectorAll(".nav-item");
-
-navItems.forEach(item => {
-  item.addEventListener("click", () => {
-    navItems.forEach(i => i.classList.remove("active"));
-    item.classList.add("active");
-  });
-});
